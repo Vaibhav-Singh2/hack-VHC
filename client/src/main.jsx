8246157f5d9f63eeab230faf8fa0fa26af3ececd
Vaibhav-Singh2 <mathura.vs@gmail.com>
2024-11-25 10:21:13 +0530

    hi2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AgoraRTC, { AgoraRTCProvider } from "agora-rtc-react";
 
-createRoot(document.getElementById('root')).render(
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AgoraRTCProvider client={client}>
+      <App />
+    </AgoraRTCProvider>
+  </StrictMode>
+);
